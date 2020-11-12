5f74b3f6c01466c7606cfab6aa2113c791234cab
Lelegithub <sonic_blu_2002@yahoo.it>
2020-11-12 14:39:13 +0100

    initial commit

---
+++ Assets/RestClient/Scripts/RestClientAPICallExample.cs
@@ -0,0 +1,35 @@
+﻿using RestClient.Core;
+using RestClient.Core.Models;
+using UnityEngine;
+
+public class RestClientAPICallExample : MonoBehaviour
+{
+    [SerializeField]
+    private string baseUrl = "https://localhost:8000";
+
+    void Start()
+    {
+        // send a get request
+        StartCoroutine(RestWebClient.Instance.HttpGet($"{baseUrl}", (r) => OnRequestComplete(r)));
+
+        // setup the request header
+         RequestHeader header = new RequestHeader {
+            Key = "Content-Type",
+            Value = "application/json"
+        };
+
+      
+    }
+
+    void OnRequestComplete(Response response)
+    {
+        Debug.Log($"Status Code: {response.StatusCode}");
+        Debug.Log($"Data: {response.Data}");
+        Debug.Log($"Error: {response.Error}");
+    }
+
+    public class Player
+    {
+        public string FullName;
+    }
+}
---
+++ Assets/RestClient/Scripts/RestWebClient.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using RestClient.Core.Models;
+using RestClient.Core.Singletons;
+using UnityEngine;
+using UnityEngine.Networking;
+ 
+namespace RestClient.Core
+{
+    public class RestWebClient : Singleton<RestWebClient>
+    {
+        private const string defaultContentType = "application/json";
+        public IEnumerator HttpGet(string url, System.Action<Response> callback)
+        {
+            using(UnityWebRequest webRequest = UnityWebRequest.Get(url))
+            {
+                yield return webRequest.SendWebRequest();
+                
+                if(webRequest.result== UnityWebRequest.Result.ConnectionError){
+                    callback(new Response {
+                        StatusCode = webRequest.responseCode,
+                        Error = webRequest.error,
+                    });
+                }
+                
+                if(webRequest.isDone)
+                {
+                    string data = System.Text.Encoding.UTF8.GetString(webRequest.downloadHandler.data);
+                    Debug.Log("Data: " + data);
+                    callback(new Response {
+                        StatusCode = webRequest.responseCode,
+                        Error = webRequest.error,
+                        Data = data
+                    });
+                }
+            }
+        }
+
+         
+        
+    }
+}
